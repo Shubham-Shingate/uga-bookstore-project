7436d6b571442d8c3ed7812fef38966aef4e8d7f
Shubhangi Rai <sr94042@uga.edu>
2021-07-26 06:30:36 -0400

    Implemented client side validation for address and payment on registration

---
+++ forwords/src/main/resources/static/js/validation.js
@@ -0,0 +1,55 @@
+function validate(){
+var f=document.getElementById("registrationForm");
+
+var hasPaymentError = paymentValidate(f);
+var hasAddressError = addressValidate(f);
+
+if(!hasPaymentError || !hasAddressError)
+return false;
+else
+return true;
+}
+
+
+
+function  paymentValidate(form){
+
+var error = document.getElementById("paymentError");
+
+var nameOnCard=form["nameOnCard"].value;
+var cardNumber=form["cardNumber"].value;
+var cardExpiry=form["cardExpiry"].value;
+var cvv=form["cvv"].value;
+var cardType=form["cardType"].value;
+
+if((nameOnCard.length > 0 && cardNumber.length > 0 && cardExpiry.length > 0 && cvv.length > 0 && cardType.length > 0) || (
+	nameOnCard.length == 0 && cardNumber.length == 0 && cardExpiry.length == 0 && cvv.length == 0 && cardType.length == 0
+)) {
+	return true;
+}
+else {
+	error.innerHTML = "Please complete each field for payment or \n Leave all fields for payment as blank"
+	return false;
+}
+}
+
+function  addressValidate(form){
+
+var error = document.getElementById("addressError");
+
+var city=form["city"].value;
+var state=form["state"].value;
+var street=form["street"].value;
+var zipcode=form["zipcode"].value;
+
+if((city.length > 0 && state.length > 0 && street.length > 0 && zipcode.length > 0 ) || (
+	city.length == 0 && state.length == 0 && street.length == 0 && zipcode.length == 0 
+)) {
+	return true;
+}
+else {
+	error.innerHTML = "Please complete each field for address or \n Leave all fields for address as blank\n \t"
+	return false;
+}
+}
+
