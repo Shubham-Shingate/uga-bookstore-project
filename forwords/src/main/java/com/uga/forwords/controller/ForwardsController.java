0e51f45cfed25371d4c643ebe38b8cf998840e40
Shubhangi Rai <sr94042@uga.edu>
2021-08-01 01:38:32 -0400

    Updated controller for Customer Account overview

--- forwords/src/main/java/com/uga/forwords/controller/ForwardsController.java
+++ forwords/src/main/java/com/uga/forwords/controller/ForwardsController.java
@@ -227,6 +227,32 @@ public class ForwardsController {
 		return "search";
 	}
 	
+	/** -----------------------------------Services for Customer Account  Overview----------------------------------- */
+	
+	@GetMapping("/customer/account-overview")
+	public String showAccountOverviewPage(Principal principal, Model model) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.add("accountId", principal.getName());
+		
+		// Make Request to backend service for profile information
+		HttpEntity<Object> profileEntity = new HttpEntity<Object>(headers);
+		ResponseEntity<PersonalDetailsResponse> profileDetailsServiceResponse = restTemplate.exchange("http://profile-detail-service/getPersonalDetails",
+				HttpMethod.GET, profileEntity, PersonalDetailsResponse.class);
+	
+		
+		// Make Request to backend service for address details
+		HttpEntity<Object> shippingEntity = new HttpEntity<Object>(headers);
+		ResponseEntity<ShippingInfoResponse> shippingDetailsServiceResponse = restTemplate.exchange("http://shipping-detail-service/getShippingDetails",
+																						HttpMethod.GET, shippingEntity, ShippingInfoResponse.class);		
+		
+		// Add information to model
+		model.addAttribute("activeUserDetails", profileDetailsServiceResponse.getBody().getUserDetails());
+		model.addAttribute("currentShippingInfo", shippingDetailsServiceResponse.getBody().getAddresses());
+		return "customer-account-overview";
+		
+	}
+	
 	/** -----------------------------------Services for showing/managing Profile Settings and related services (calls go to profile_detail_service)----------------------------------- */
 	
 	@GetMapping("/customer/showSettingsPage")
@@ -684,31 +710,5 @@ public class ForwardsController {
 		}
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 }
